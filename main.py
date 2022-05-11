0ed39826ad3de92b1754cb1cd2687d5ba903e738
pearl178 <89487585+pearl178@users.noreply.github.com>
2022-05-10 18:36:52 -0600

    eliminate some lines with list comprehension

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ correct_states = []
 while len(correct_states) < 50:
     answer_state = screen.textinput(title=f"{len(correct_states)}/50 Guess the state", prompt="What's another state's name?")
     if answer_state.lower() == 'exit':
+        states_to_learn = [state for state in states if state not in correct_states]
+        learn_states = pandas.DataFrame(states_to_learn)
+        learn_states.to_csv("states_to_learn.csv")
         break
     guess = answer_state.title()
     if guess in states:
@@ -36,11 +39,4 @@ while len(correct_states) < 50:
         state_name.goto(position)
         state_name.write(guess, align="center", font=("Arial", 10, "normal"))
 
-states_to_learn = []
-for state in states:
-    if state not in correct_states:
-        states_to_learn.append(state)
-
-learn_states = pandas.DataFrame(states_to_learn)
-learn_states.to_csv("states_to_learn.csv")
 
